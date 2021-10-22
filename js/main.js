05473786dad80be53f404f4405c1e93bc6f28d6e
h584974 <oliveroloughlin@hotmail.com>
2021-10-22 14:50:56 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,392 @@
+import {
+    PerspectiveCamera,
+    WebGLRenderer,
+    PCFSoftShadowMap,
+    Scene,
+    Mesh,
+    TextureLoader,
+    RepeatWrapping,
+    DirectionalLight,
+    Vector3,
+    AxesHelper,
+} from './lib/three.module.js';
+
+import Utilities from './lib/Utilities.js';
+import MouseLookController from './controls/MouseLookController.js';
+
+import TextureSplattingMaterial from './materials/TextureSplattingMaterial.js';
+import TerrainBufferGeometry from './terrain/TerrainBufferGeometry.js';
+import { GLTFLoader } from './loaders/GLTFLoader.js';
+import { SimplexNoise } from './lib/SimplexNoise.js';
+
+async function main() {
+
+    const scene = new Scene();
+
+    const axesHelper = new AxesHelper(15);
+    scene.add(axesHelper);
+
+    const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+    const renderer = new WebGLRenderer({ antialias: true });
+    renderer.setClearColor(0xffffff);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMap.type = PCFSoftShadowMap;
+
+    /**
+     * Handle window resize:
+     *  - update aspect ratio.
+     *  - update projection matrix
+     *  - update renderer size
+     */
+    window.addEventListener('resize', () => {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+
+        renderer.setSize(window.innerWidth, window.innerHeight);
+    }, false);
+
+    /**
+     * Add canvas element to DOM.
+     */
+    document.body.appendChild(renderer.domElement);
+
+    /**
+     * Add light
+     */
+    const directionalLight = new DirectionalLight(0xffffff);
+    directionalLight.position.set(3000, 4000, 0);
+    directionalLight.castShadow = true;
+
+    //Set up shadow properties for the light
+    directionalLight.shadow.mapSize.width = 512;
+    directionalLight.shadow.mapSize.height = 512;
+    directionalLight.shadow.camera.near = 0.5;
+    directionalLight.shadow.camera.far = 2000;
+
+    scene.add(directionalLight);
+
+    // Set direction
+    directionalLight.target.position.set(0, 15, 0);
+    scene.add(directionalLight.target);
+
+    camera.position.z = 70;
+    camera.position.y = 55;
+    camera.rotation.x -= Math.PI * 0.25;
+
+
+    /**
+     * Add terrain:
+     * 
+     * We have to wait for the image file to be loaded by the browser.
+     * There are many ways to handle asynchronous flow in your application.
+     * We are using the async/await language constructs of Javascript:
+     *  - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function
+     */
+    const heightmapImage = await Utilities.loadImage('resources/images/custom_heightmap2.png');
+    const width = 100;
+
+    const simplex = new SimplexNoise();
+    const terrainGeometry = new TerrainBufferGeometry({
+        width,
+        heightmapImage,
+        noiseFn: simplex.noise.bind(simplex),
+        numberOfSubdivisions: 64,
+        height: 20
+    });
+
+    const grassTexture1 = new TextureLoader().load('resources/textures/grass_01.jpg');
+    grassTexture1.wrapS = RepeatWrapping;
+    grassTexture1.wrapT = RepeatWrapping;
+    grassTexture1.repeat.set(5000 / width, 5000 / width);
+
+    const grassTexture = new TextureLoader().load('resources/textures/grass_02.png');
+    grassTexture.wrapS = RepeatWrapping;
+    grassTexture.wrapT = RepeatWrapping;
+    grassTexture.repeat.set(5000 / width, 5000 / width);
+
+    const rockTexture = new TextureLoader().load('resources/textures/rock_02.png');
+    rockTexture.wrapS = RepeatWrapping;
+    rockTexture.wrapT = RepeatWrapping;
+    rockTexture.repeat.set(1500 / width, 1500 / width);
+
+    const snowyRockTexture = new TextureLoader().load('resources/textures/snowy_rock_01.png');
+    snowyRockTexture.wrapS = RepeatWrapping;
+    snowyRockTexture.wrapT = RepeatWrapping;
+    snowyRockTexture.repeat.set(1500 / width, 1500 / width);
+
+
+    const splatMap = new TextureLoader().load('resources/images/custom_heightmap2.png');
+
+    const terrainMaterial = new TextureSplattingMaterial({
+        color: 0xffffff,
+        shininess: 0,
+        textures: [grassTexture, snowyRockTexture],
+        splatMaps: [splatMap]
+    });
+
+    const terrain = new Mesh(terrainGeometry, terrainMaterial);
+
+    terrain.castShadow = true;
+    terrain.receiveShadow = true;
+
+    scene.add(terrain);
+
+    /**
+     * Add trees
+     */
+
+    // instantiate a GLTFLoader:
+    const loader = new GLTFLoader();
+
+    loader.load(
+        // resource URL
+        'resources/models/kenney_nature_kit/tree_thin.glb',
+        // called when resource is loaded
+        (object) => {
+            for (let x = -width/2; x < width/2; x += 1.3) {
+                for (let z = -width/2; z < width/2; z += 1.3) {
+                    
+                    const px = x + 1 + (6 * Math.random()) - 3;
+                    const pz = z + 1 + (6 * Math.random()) - 3;
+
+                    const height = terrainGeometry.getHeightAt(px, pz);
+                    const heightL = terrainGeometry.getHeightAt(px - 1, pz);
+                    const heightR = terrainGeometry.getHeightAt(px + 1, pz);
+                    const heightB = terrainGeometry.getHeightAt(px, pz - 1);
+                    const heightF = terrainGeometry.getHeightAt(px, pz + 1);
+
+                    const diffL = Math.abs(height - heightL);
+                    const diffR = Math.abs(height - heightR);
+                    const diffB = Math.abs(height - heightB);
+                    const diffF = Math.abs(height - heightF);
+
+                    if (height > 0.35 * terrainGeometry.height && height < 0.75 * terrainGeometry.height && diffL < 0.07 && diffR < 0.07 && diffB < 0.07 && diffF < 0.07) {
+                        const tree = object.scene.children[0].clone();
+
+                        tree.traverse((child) => {
+                            if (child.isMesh) {
+                                child.castShadow = true;
+                                child.receiveShadow = true;
+                            }
+                        });
+                        
+                        tree.position.x = px;
+                        tree.position.y = height - 0.01;
+                        tree.position.z = pz;
+
+                        tree.rotation.y = Math.random() * (2 * Math.PI);
+
+                        tree.scale.multiplyScalar(0.2 + Math.random() * 3);
+
+                        scene.add(tree);
+                    }
+
+                }
+            }
+        },
+        (xhr) => {
+            console.log(((xhr.loaded / xhr.total) * 100) + '% loaded');
+        },
+        (error) => {
+            console.error('Error loading model.', error);
+        }
+    );
+
+    loader.load(
+        // resource URL
+        'resources/models/flower/scene.gltf',
+        // called when resource is loaded
+        (object) => {
+            for (let x = -width/2; x < width/2; x += 5) {
+                for (let z = -width/2; z < width/2; z += 5) {
+                    
+                    const px = x + 1 + (6 * Math.random()) - 3;
+                    const pz = z + 1 + (6 * Math.random()) - 3;
+
+                    const height = terrainGeometry.getHeightAt(px, pz);
+
+                    if (height > 0 && height < 3) {
+                        const flower = object.scene.children[0].clone();
+
+                        flower.traverse((child) => {
+                            if (child.isMesh) {
+                                child.castShadow = true;
+                                child.receiveShadow = true;
+                            }
+                        });
+                        
+                        flower.position.x = px;
+                        flower.position.y = height - 0.01;
+                        flower.position.z = pz;
+
+                        flower.rotation.y = Math.random() * (Math.PI / 10);
+
+                        flower.scale.multiplyScalar(Math.random() * 0.5);
+
+                        scene.add(flower);
+                    }
+
+                }
+            }
+        },
+        (xhr) => {
+            console.log(((xhr.loaded / xhr.total) * 100) + '% loaded');
+        },
+        (error) => {
+            console.error('Error loading model.', error);
+        }
+    );
+
+    /**
+     * Set up camera controller:
+     */
+
+    const mouseLookController = new MouseLookController(camera);
+
+    // We attach a click lister to the canvas-element so that we can request a pointer lock.
+    // https://developer.mozilla.org/en-US/docs/Web/API/Pointer_Lock_API
+    const canvas = renderer.domElement;
+
+    canvas.addEventListener('click', () => {
+        canvas.requestPointerLock();
+    });
+
+    let yaw = 0;
+    let pitch = 0;
+    const mouseSensitivity = 0.001;
+
+    function updateCamRotation(event) {
+        yaw += event.movementX * mouseSensitivity;
+        pitch += event.movementY * mouseSensitivity;
+    }
+
+    document.addEventListener('pointerlockchange', () => {
+        if (document.pointerLockElement === canvas) {
+            canvas.addEventListener('mousemove', updateCamRotation, false);
+        } else {
+            canvas.removeEventListener('mousemove', updateCamRotation, false);
+        }
+    });
+
+    let move = {
+        forward: false,
+        backward: false,
+        left: false,
+        right: false,
+        speed: 0.05
+    };
+
+    window.addEventListener('keydown', (e) => {
+        if (e.code === 'KeyW') {
+            move.forward = true;
+            e.preventDefault();
+        } else if (e.code === 'KeyS') {
+            move.backward = true;
+            e.preventDefault();
+        } else if (e.code === 'KeyA') {
+            move.left = true;
+            e.preventDefault();
+        } else if (e.code === 'KeyD') {
+            move.right = true;
+            e.preventDefault();
+        }
+    });
+
+    window.addEventListener('keyup', (e) => {
+        if (e.code === 'KeyW') {
+            move.forward = false;
+            e.preventDefault();
+        } else if (e.code === 'KeyS') {
+            move.backward = false;
+            e.preventDefault();
+        } else if (e.code === 'KeyA') {
+            move.left = false;
+            e.preventDefault();
+        } else if (e.code === 'KeyD') {
+            move.right = false;
+            e.preventDefault();
+        }
+    });
+
+    const velocity = new Vector3(0.0, 0.0, 0.0);
+
+    const timeMax = 2400;
+    const timeSpeed = 0.1;
+    const lightDistance = 1000;
+    let nowPI = 0;
+    let sinx = 0;
+    let siny = -1;
+    let time = 0;
+    let timeFraction = 0;
+    let lightPosY;
+    let lightPosX;
+
+    let then = performance.now();
+    function loop(now) {
+
+        const delta = now - then;
+        then = now;
+
+        time += delta * timeSpeed;
+
+        if (time > timeMax) time = 0;
+
+        if (time == 0) {
+            timeFraction = 0;
+        } else {
+            timeFraction = time / timeMax;
+        }
+
+        nowPI = timeFraction * 2 * Math.PI;
+
+        sinx = Math.sin(nowPI);
+        siny = Math.sin(nowPI - 1/2*Math.PI);
+
+        lightPosX = lightDistance * sinx;
+        lightPosY = lightDistance * siny;
+
+        directionalLight.position.set(lightPosX, lightPosY);
+
+        const moveSpeed = move.speed * delta;
+
+        velocity.set(0.0, 0.0, 0.0);
+
+        if (move.left) {
+            velocity.x -= moveSpeed;
+        }
+
+        if (move.right) {
+            velocity.x += moveSpeed;
+        }
+
+        if (move.forward) {
+            velocity.z -= moveSpeed;
+        }
+
+        if (move.backward) {
+            velocity.z += moveSpeed;
+        }
+
+        // update controller rotation.
+        mouseLookController.update(pitch, yaw);
+        yaw = 0;
+        pitch = 0;
+
+        // apply rotation to velocity vector, and translate moveNode with it.
+        velocity.applyQuaternion(camera.quaternion);
+        camera.position.add(velocity);
+
+        // render scene:
+        renderer.render(scene, camera);
+
+        requestAnimationFrame(loop);
+
+    };
+
+    loop(performance.now());
+
+}
+
+main(); // Start application
