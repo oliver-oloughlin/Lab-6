a9c5e050af1ec17feaf8b76b01361cf4eb77f3b1
h584974 <oliveroloughlin@hotmail.com>
2021-11-04 05:38:40 +0100

    Migrated code for loading models to ModelLoader

--- js/main.js
+++ js/main.js
@@ -12,6 +12,7 @@ import {
     SphereBufferGeometry,
 } from './lib/three.module.js';
 
+import ModelLoader from './lib/ModelLoader.js'
 import Utilities from './lib/Utilities.js';
 import MouseLookController from './controls/MouseLookController.js';
 import TextureSplattingMaterial from './materials/TextureSplattingMaterial.js';
@@ -133,120 +134,16 @@ async function main() {
     scene.add(skybox);
     skybox.position.y = 0; // fjern når ferdig med testing
 
-    /**
-     * Add trees
-     */
-
-    // instantiate a GLTFLoader:
+    // instantiate a GLTFLoader and load all models
     const loader = new GLTFLoader();
-
-    loader.load(
-        // resource URL
-        'resources/models/kenney_nature_kit/tree_thin.glb',
-        // called when resource is loaded
-        (object) => {
-            for (let x = -width/2; x < width/2; x += 1.3) {
-                for (let z = -width/2; z < width/2; z += 1.3) {
-                    
-                    const px = x + 1 + (6 * Math.random()) - 3;
-                    const pz = z + 1 + (6 * Math.random()) - 3;
-
-                    const height = terrainGeometry.getHeightAt(px, pz);
-                    const heightL = terrainGeometry.getHeightAt(px - 1, pz);
-                    const heightR = terrainGeometry.getHeightAt(px + 1, pz);
-                    const heightB = terrainGeometry.getHeightAt(px, pz - 1);
-                    const heightF = terrainGeometry.getHeightAt(px, pz + 1);
-
-                    const diffL = Math.abs(height - heightL);
-                    const diffR = Math.abs(height - heightR);
-                    const diffB = Math.abs(height - heightB);
-                    const diffF = Math.abs(height - heightF);
-
-                    if (height > 0.35 * terrainGeometry.height && height < 0.75 * terrainGeometry.height && diffL < 0.07 && diffR < 0.07 && diffB < 0.07 && diffF < 0.07) {
-                        const tree = object.scene.children[0].clone();
-
-                        tree.traverse((child) => {
-                            if (child.isMesh) {
-                                child.castShadow = true;
-                                child.receiveShadow = true;
-                            }
-                        });
-                        
-                        tree.position.x = px;
-                        tree.position.y = height - 0.01;
-                        tree.position.z = pz;
-
-                        tree.rotation.y = Math.random() * (2 * Math.PI);
-
-                        tree.scale.multiplyScalar(0.2 + Math.random() * 3);
-
-                        scene.add(tree);
-                    }
-
-                }
-            }
-        },
-        (xhr) => {
-            console.log(((xhr.loaded / xhr.total) * 100) + '% loaded');
-        },
-        (error) => {
-            console.error('Error loading model.', error);
-        }
-    );
-
-    loader.load(
-        // resource URL
-        'resources/models/flower/scene.gltf',
-        // called when resource is loaded
-        (object) => {
-            for (let x = -width/2; x < width/2; x += 5) {
-                for (let z = -width/2; z < width/2; z += 5) {
-                    
-                    const px = x + 1 + (6 * Math.random()) - 3;
-                    const pz = z + 1 + (6 * Math.random()) - 3;
-
-                    const height = terrainGeometry.getHeightAt(px, pz);
-
-                    if (height > 0 && height < 3) {
-                        const flower = object.scene.children[0].clone();
-
-                        flower.traverse((child) => {
-                            if (child.isMesh) {
-                                child.castShadow = true;
-                                child.receiveShadow = true;
-                            }
-                        });
-                        
-                        flower.position.x = px;
-                        flower.position.y = height - 0.01;
-                        flower.position.z = pz;
-
-                        flower.rotation.y = Math.random() * (Math.PI / 10);
-
-                        flower.scale.multiplyScalar(Math.random() * 0.5);
-
-                        scene.add(flower);
-                    }
-
-                }
-            }
-        },
-        (xhr) => {
-            console.log(((xhr.loaded / xhr.total) * 100) + '% loaded');
-        },
-        (error) => {
-            console.error('Error loading model.', error);
-        }
-    );
-
-    /**
-     * Set up camera controller:
-     */
+    ModelLoader.loadAllModels(scene,loader,width,terrainGeometry);
 
     const canvas = renderer.domElement;
     const mouseLookController = new MouseLookController(camera, canvas, window, document);
 
-    const timeSpeed = 24 * 60 * 4;
+
+    // Setup timeCycleController
+    const timeSpeed = 24 * 60 * 4; // 1 day-cycle = 15 seconds, default = realtime
     const lightDistance = 1000;
     const timeCycleController = new TimeCycleController(timeSpeed, lightDistance, directionalLight);
 
